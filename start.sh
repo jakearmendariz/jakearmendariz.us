ae4965add4da5e0927ef8d93b903ec25eef3bf37
Jake Armendariz <jakearmendariz99@gmail.com>
2020-11-13 20:10:26 -0800

    start bash

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 app="portfolio"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
+docker run -p 8081:80 \
   --name=${app} \
   -v "$PWD:/app" ${app}
+wait
+docker stop ${app}
+docker rm ${app}
